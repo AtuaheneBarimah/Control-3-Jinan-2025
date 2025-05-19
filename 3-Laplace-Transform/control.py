2aa4c14e2d39886c6a3509787e77266eb34ac4a1
Barimah <ABARIM300@caledonian.ac.uk>
2025-05-19 13:23:54 +0100

    Control 3 (Chapter 3) commit: 2025-05-19

--- 3-Laplace-Transform/control.py
+++ 3-Laplace-Transform/control.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 import scipy.signal as signal
 import sympy as sp
 import control
+
+sp.init_printing()
+
+t= sp.symbols('t')
+s= sp.symbols('s')
+b= sp.symbols('b')
+
+
