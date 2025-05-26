afce7dd2b3060a4b4e0feeefe511eb5e1a84e978
Barimah <ABARIM300@caledonian.ac.uk>
2025-05-26 08:29:15 +0800

    Control 3 (Chapter 1-Update) commit: 2025-05-26

--- 1-Open-Closed-Loop-Systems/feedback.py
+++ 1-Open-Closed-Loop-Systems/feedback.py
@@ -4,13 +4,13 @@ import scipy.signal as signal
 
 ref = 55
 #r = ref*(np.ones(100))  # Step input
-r = np.linspace(ref, ref/ref, 100)  # Step input (Decreasing)
+#r = np.linspace(ref, ref/ref, 100)  # Step input (Decreasing)
 #r = np.linspace(ref/ref, ref, 100)  # Step input (Increasing)
-#r = ref*(signal.square(1 * np.pi * 0.1 * np.arange(100)))  # Square wave input
-#r = ref*(signal.sawtooth(1 * np.pi * 0.1 * np.arange(100)))  # Sawtooth wave input
-#r = ref*(signal.sawtooth(1 * np.pi * 0.1 * np.arange(100), 0.5))  # Triangular wave input
-#r = ref*(signal.chirp(np.arange(100), 0, 10, 1))  # Chirp signal input
-#r = ref*(np.sin(1 * np.pi * 0.1 * np.arange(100)) + 1)  # Sine wave input
+r = (ref*signal.square(1 * np.pi * 0.1 * np.arange(100)))  # Square wave input
+#r = (ref*signal.sawtooth(1 * np.pi * 0.1 * np.arange(100)))  # Sawtooth wave input
+#r = (ref*signal.sawtooth(1 * np.pi * 0.1 * np.arange(100), 0.5))  # Triangular wave input
+#r = (ref*signal.chirp(np.arange(100), 0, 10, 1))  # Chirp signal input
+#r = (ref*np.sin(1 * np.pi * 0.1 * np.arange(100)) + 1)  # Sine wave input
 
 car  = 10
 reference_signal = r 
